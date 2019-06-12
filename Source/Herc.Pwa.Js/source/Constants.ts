3a714e881c1030670843f56fe7877a8c4816f1e3
stackbaxter <stackbaxter@yahoo.com>
2019-06-12 02:12:24 +0000

    to Synchronize the pwa's

--- Source/Herc.Pwa.Js/source/Constants.ts
+++ Source/Herc.Pwa.Js/source/Constants.ts
@@ -7,7 +7,6 @@ export const ReduxDevToolsFactoryName: string = 'ReduxDevToolsFactory';
 export const ReduxDevToolsName: string = "reduxDevTools";
 export const ReduxExtentionName: string = '__REDUX_DEVTOOLS_EXTENSION__';
 
-
 export const DotNetActionQualifiedNames = {
   UpdateEdgeAccountAction: "Herc.Pwa.Client.Features.Edge.EdgeAccount.UpdateEdgeAccountAction, Herc.Pwa.Client, Version = 1.0.0.0, Culture = neutral, PublicKeyToken = null",
   UpdateEdgeCurrencyWalletAction: "Herc.Pwa.Client.Features.Edge.EdgeCurrencyWallet.UpdateEdgeCurrencyWalletAction, Herc.Pwa.Client, Version = 1.0.0.0, Culture = neutral, PublicKeyToken = null",
