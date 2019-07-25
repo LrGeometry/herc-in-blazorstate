079d52c930a9c9d7271dcc254bbae34c909b8164
Steven T. Cramer <SteveTCramer@gmail.com>
2019-07-25 01:19:16 +0000

    Use the new serializer in System.Text.Json

--- Test/Herc.Pwa.Client.Integration.Tests/Misc/JsonSerializationTests.cs
+++ Test/Herc.Pwa.Client.Integration.Tests/Misc/JsonSerializationTests.cs
@@ -1,16 +1,15 @@
 ﻿namespace Herc.Pwa.Server.Integration.Tests.Misc
 {
-  using Microsoft.JSInterop;
   using Herc.Pwa.Client.Features.Edge.EdgeCurrencyWallet;
-  using System.IO;
   using System;
+  using System.Text.Json.Serialization;
 
   class JsonSerializationTests
   {
     public void GoodSingle()
     {
       string json = System.IO.File.ReadAllText(@".\TestData\SerializationTests\GoodSingle.json");
-      UpdateEdgeCurrencyWalletAction updateEdgeCurrencyWalletAction = Json.Deserialize<UpdateEdgeCurrencyWalletAction>(json);
+      UpdateEdgeCurrencyWalletAction updateEdgeCurrencyWalletAction = JsonSerializer.Parse<UpdateEdgeCurrencyWalletAction>(json);
     }
 
     // Skip: By making private will skip the test
@@ -20,7 +19,7 @@
       // But given we don't care about the field we now remove it prior to serialization
       string currentDirectory = Environment.CurrentDirectory;
       string json = System.IO.File.ReadAllText(@".\TestData\SerializationTests\BadSingle.json");
-      UpdateEdgeCurrencyWalletAction updateEdgeCurrencyWalletAction = Json.Deserialize<UpdateEdgeCurrencyWalletAction>(json);
+      UpdateEdgeCurrencyWalletAction updateEdgeCurrencyWalletAction = JsonSerializer.Parse<UpdateEdgeCurrencyWalletAction>(json);
     }
 
   }
