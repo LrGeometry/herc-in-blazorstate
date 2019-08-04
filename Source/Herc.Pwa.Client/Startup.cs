fa3fccbc5700af69205a62c8cc1c4d246ec34b73
Steven T. Cramer <Steve.Cramer@TimeWarp.Enterprises>
2019-08-04 17:12:26 +0000

    Remove Redux Dev Tools
    This will improve production.  It would be nice to make this a compiler option. But not a priority.

--- Source/Herc.Pwa.Client/Startup.cs
+++ Source/Herc.Pwa.Client/Startup.cs
@@ -1,54 +1,57 @@
 ﻿namespace Herc.Pwa.Client
 {
-    using Herc.Pwa.Client.Features.Application;
-    using BlazorState;
-    using FluentValidation;
-    using Herc.Pwa.Client.Services;
-    using Herc.Pwa.Client.Components.Shared;
-    using Herc.Pwa.Client.Features.Edge.EdgeCurrencyWallet;
-    using Microsoft.AspNetCore.Components.Builder;
-    using Microsoft.Extensions.DependencyInjection;
-    using System.Reflection;
-    using System.Text.Json;
-    using BlazorHostedCSharp.Client.Features.ClientLoader;
-    using Nethereum.Util;
-    using Herc.Pwa.Client.Features.Edge;
-    using Herc.Pwa.Client.Features.Edge.EdgeAccount;
+  using Herc.Pwa.Client.Features.Application;
+  using BlazorState;
+  using FluentValidation;
+  using Herc.Pwa.Client.Services;
+  using Herc.Pwa.Client.Components.Shared;
+  using Microsoft.AspNetCore.Components.Builder;
+  using Microsoft.Extensions.DependencyInjection;
+  using System.Reflection;
+  using System.Text.Json;
+  using BlazorHostedCSharp.Client.Features.ClientLoader;
+  using Nethereum.Util;
+  using Herc.Pwa.Client.Features.Edge;
+  using Herc.Pwa.Client.Features.Edge.EdgeAccount;
 
-    public class Startup
+  public class Startup
+  {
+    public void Configure(IComponentsApplicationBuilder aComponentsApplicationBuilder) =>
+      aComponentsApplicationBuilder.AddComponent<App>("app");
+
+    public void ConfigureServices(IServiceCollection aServiceCollection)
     {
-        public void Configure(IComponentsApplicationBuilder aComponentsApplicationBuilder) =>
-          aComponentsApplicationBuilder.AddComponent<App>("app");
-
-        public void ConfigureServices(IServiceCollection aServiceCollection)
-        {
-            aServiceCollection.AddBlazorState
-            (
-              (aOptions) => aOptions.Assemblies =
-                new Assembly[]
-                {
-            typeof(Startup).GetTypeInfo().Assembly,
-                }
-            );
-            aServiceCollection.AddSingleton<ColorPalette>();
-            aServiceCollection.AddSingleton<AmountConverter>();
-            aServiceCollection.AddSingleton<AddressUtil>();
-            aServiceCollection.AddSingleton
-            (
-              new JsonSerializerOptions
+      aServiceCollection.AddBlazorState
+      (
+        (aOptions) =>
+          {
+            aOptions.UseReduxDevToolsBehavior = false;
+            aOptions.Assemblies =
+              new Assembly[]
               {
-                  PropertyNamingPolicy = JsonNamingPolicy.CamelCase
-              }
-            );
-
-            aServiceCollection.AddScoped<IValidator<SendAction>, SendValidator>();
-            aServiceCollection.AddScoped<ClientLoader>();
-            aServiceCollection.AddScoped<IClientLoaderConfiguration, ClientLoaderConfiguration>();
-
-            aServiceCollection.AddTransient<ApplicationState>();
-            aServiceCollection.AddTransient<EdgeState>();
-            aServiceCollection.AddTransient<EdgeAccountState>();
-            aServiceCollection.AddTransient<EdgeCurrencyWalletsState>();
+                typeof(Startup).GetTypeInfo().Assembly,
+              };
+          }
+      );
+      aServiceCollection.AddSingleton<ColorPalette>();
+      aServiceCollection.AddSingleton<AmountConverter>();
+      aServiceCollection.AddSingleton<AddressUtil>();
+      aServiceCollection.AddSingleton
+      (
+        new JsonSerializerOptions
+        {
+          PropertyNamingPolicy = JsonNamingPolicy.CamelCase
         }
+      );
+
+      aServiceCollection.AddScoped<IValidator<SendAction>, SendValidator>();
+      aServiceCollection.AddScoped<ClientLoader>();
+      aServiceCollection.AddScoped<IClientLoaderConfiguration, ClientLoaderConfiguration>();
+
+      aServiceCollection.AddTransient<ApplicationState>();
+      aServiceCollection.AddTransient<EdgeState>();
+      aServiceCollection.AddTransient<EdgeAccountState>();
+      aServiceCollection.AddTransient<EdgeCurrencyWalletsState>();
     }
+  }
 }
