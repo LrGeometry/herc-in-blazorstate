aa081eb7a96e47b1d4523b43f134ab52f06c07bd
Steven T. Cramer <Steve.Cramer@TimeWarp.Enterprises>
2019-08-05 08:42:04 +0000

    Add TestEthAccount
    And the test to prove it "works"

---
+++ Source/Herc.Pwa.Shared/Constants/AccountAddresses/TestEthAccounts.cs
@@ -0,0 +1,18 @@
+﻿namespace Herc.Pwa.Shared.Constants.AccountAddresses
+{
+  using Nethereum.Web3.Accounts;
+
+  public class TestEthAccount
+  {
+    internal const string TestEthPrivateKey = "307F685A376C5BF8296B4BE1D3703F068315BCD3115280B52C4CA0F8BA83C474";
+
+    public const string TestEthAccountAddress = "0x12833d6fADd206dEd2fcE84936d8D78326AB8EfA";
+
+    public TestEthAccount()
+    {
+      TesterAcct = new Account(TestEthPrivateKey);
+    }
+
+    public Account TesterAcct { get; set; }
+  }
+}
