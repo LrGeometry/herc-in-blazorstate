8d88d189529e9c9a4dd47e4262055441eae1b370
Steven T. Cramer <Steve.Cramer@TimeWarp.Enterprises>
2019-08-11 15:14:56 +0000

    Move EthereumSettings into appsettings for now.  In production they will need tobe configured on the Azure server so we don't leak the private keys

---
+++ Source/Herc.Pwa.Server/Configuration/EthereumSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace Herc.Pwa.Server.Configuration
+{
+  public class EthereumSettings
+  {
+    public string Endpoint { get; set; }
+    public string TestAccountAddress { get; set; }
+    public string TestAccountPrivateKey { get; set; }
+    public string NftCreatorAddress { get; set; }
+  }
+}
---
+++ Source/Herc.Pwa.Server/Constants/EthereumConstants.cs
@@ -0,0 +1,21 @@
+﻿//namespace Herc.Pwa.Server.Constants.Ethereum
+//{
+//  public static class EthereumConstants
+//  {
+//    // Ganache Local
+//    public const string GanacheEndpoint = "https://127.0.0.7/";
+//    public const string GanacheTestEthAccountAddress = "0x356dbD67D52c8292D27E6Dec70c2B03dC5D4E8a5";
+//    public const string GanacheTestEthPrivateKey = "e69fc041cb3d7daca5c53196595d822f1cb500ea38c3d4805b10d556db100709";
+
+
+//    // Rinkeby
+//    public const string RinkebyEndpoint = "https://rinkeby.infura.io/v3/92ffd0e0fb6e484198387a5fabf31954";
+//    public const string RinkebyTestEthAccountAddress = "0x12833d6fADd206dEd2fcE84936d8D78326AB8EfA";
+//    public const string RinkebyTestEthPrivateKey = "307F685A376C5BF8296B4BE1D3703F068315BCD3115280B52C4CA0F8BA83C474";
+
+//    // Ropsten
+//    public const string RopstenEndpoint = "https://ropsten.infura.io/v3/92ffd0e0fb6e484198387a5fabf31954";
+//    public const string RopstenTestEthAccountAddress = "0xd45BFdC8de1f5892c6Cddf2782Bc657ce270fE20";
+//    public const string RopstenTestEthPrivateKey = "C55D5729F7647AA68401226912294B97610F3958AC00C51A6CE31E5BD10BFE8A";
+//  }
+//}
